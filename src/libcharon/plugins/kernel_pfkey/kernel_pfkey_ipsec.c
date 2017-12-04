magicknight/strongswan

commit 88a8fba1c76eda1c39dce4d0b2038c760f6d6140

kernel-pfkey: Support anti-replay windows > 2k
FreeBSD 11.1 supports a new extension to configure larger anti-replay
windows, now configured as number of packets.

Fixes #2461.

src/libcharon/plugins/kernel_pfkey/kernel_pfkey_ipsec.c
@@ -1740,7 +1740,7 @@ METHOD(kernel_ipsec_t, add_sa, status_t,
 #ifdef __linux__
 			sa->sadb_sa_replay = min(data->replay_window, 32);
 #else
-			sa->sadb_sa_replay = (data->replay_window + 7) / 8;
+			sa->sadb_sa_replay = min((data->replay_window + 7) / 8, UINT8_MAX);
 #endif
 		}
 		sa->sadb_sa_auth = lookup_algorithm(INTEGRITY_ALGORITHM, data->int_alg);
@@ -1748,6 +1748,19 @@ METHOD(kernel_ipsec_t, add_sa, status_t,
 											   data->enc_alg);
 	}
 	PFKEY_EXT_ADD(msg, sa);
+
+#ifdef SADB_X_EXT_SA_REPLAY
+	if (data->inbound)
+	{
+		struct sadb_x_sa_replay *replay;
+
+		replay = (struct sadb_x_sa_replay*)PFKEY_EXT_ADD_NEXT(msg);
+		replay->sadb_x_replay_exttype = SADB_X_EXT_SA_REPLAY;
+		replay->sadb_x_replay_len = PFKEY_LEN(sizeof(struct sadb_x_sa_replay));
+		replay->sadb_x_replay_replay = min(data->replay_window, UINT32_MAX-32);
+		PFKEY_EXT_ADD(msg, replay);
+	}
+#endif
 
 	sa2 = (struct sadb_x_sa2*)PFKEY_EXT_ADD_NEXT(msg);
 	sa2->sadb_x_sa2_exttype = SADB_X_EXT_SA2;

commit 2c4c0ee12a9272af54960f24db8ab8e441419c2a

kernel-pfkey: Fix extended replay configuration on FreeBSD 11.1
Fixes: 88a8fba1c76e ("kernel-pfkey: Support anti-replay windows > 2k")

src/libcharon/plugins/kernel_pfkey/kernel_pfkey_ipsec.c
@@ -1752,13 +1752,13 @@ METHOD(kernel_ipsec_t, add_sa, status_t,
 #ifdef SADB_X_EXT_SA_REPLAY
 	if (data->inbound)
 	{
-		struct sadb_x_sa_replay *replay;
+		struct sadb_x_sa_replay *repl;
 
-		replay = (struct sadb_x_sa_replay*)PFKEY_EXT_ADD_NEXT(msg);
-		replay->sadb_x_replay_exttype = SADB_X_EXT_SA_REPLAY;
-		replay->sadb_x_replay_len = PFKEY_LEN(sizeof(struct sadb_x_sa_replay));
-		replay->sadb_x_replay_replay = min(data->replay_window, UINT32_MAX-32);
-		PFKEY_EXT_ADD(msg, replay);
+		repl = (struct sadb_x_sa_replay*)PFKEY_EXT_ADD_NEXT(msg);
+		repl->sadb_x_sa_replay_exttype = SADB_X_EXT_SA_REPLAY;
+		repl->sadb_x_sa_replay_len = PFKEY_LEN(sizeof(struct sadb_x_sa_replay));
+		repl->sadb_x_sa_replay_replay = min(data->replay_window, UINT32_MAX-32);
+		PFKEY_EXT_ADD(msg, repl);
 	}
 #endif
 